bd89bac70957c3f93ce8465f25492ab3f5332963
Oleg Makaruk <oleg.makaruk@gmail.com>
2023-06-20 15:27:23 +0300

    Add more TirePressureMonitoringSystem tests and correct style formatting

--- src/TirePressureMonitoringSystem.Tests/AlarmTests.cs
+++ src/TirePressureMonitoringSystem.Tests/AlarmTests.cs
@@ -2,20 +2,25 @@
 using NUnit.Framework;
 using TDDMicroExercises.TirePressureMonitoringSystem.Abstractions;
 
-namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests {
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
+{
     [TestFixture]
-    public class AlarmTests {
-        private readonly ISensor _sensor;
-        private readonly Alarm _alarm;
+    public class AlarmTests
+    {
+        private ISensor _sensor;
+        private Alarm _alarm;
 
-        public AlarmTests() {
+        [SetUp]
+        public void Setup()
+        {
             _sensor = Substitute.For<ISensor>();
             _alarm = new Alarm(_sensor);
         }
 
         [Test]
         [Category("SetAlarmOn")]
-        public void Check_PressureTooLow_ShouldSetAlarmOn() {
+        public void Check_PressureTooLow_ShouldSetAlarmOn()
+        {
             // Arrange
             _sensor.PopNextPressurePsiValue().Returns(16);
 
@@ -28,7 +33,8 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests {
 
         [Test]
         [Category("SetAlarmOn")]
-        public void Check_PressureTooHigh_ShouldSetAlarmOn() {
+        public void Check_PressureTooHigh_ShouldSetAlarmOn()
+        {
             // Arrange
             _sensor.PopNextPressurePsiValue().Returns(22);
 
@@ -41,7 +47,8 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests {
 
         [Test]
         [Category("SetAlarmOff")]
-        public void Check_PressureAtLow_ShouldNotSetAlarmOn() {
+        public void Check_PressureAtLow_ShouldNotSetAlarm()
+        {
             // Arrange
             _sensor.PopNextPressurePsiValue().Returns(17);
 
@@ -54,7 +61,8 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests {
 
         [Test]
         [Category("SetAlarmOff")]
-        public void Check_PressureNormal_ShouldNotSetAlarmOn() {
+        public void Check_PressureNormal_ShouldNotSetAlarm()
+        {
             // Arrange
             _sensor.PopNextPressurePsiValue().Returns(19);
 
@@ -67,13 +75,54 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests {
 
         [Test]
         [Category("SetAlarmOff")]
-        public void Check_PressureAtHigh_ShouldNotSetAlarmOn() {
+        public void Check_PressureAtHigh_ShouldNotSetAlarm()
+        {
             // Arrange
             _sensor.PopNextPressurePsiValue().Returns(21);
 
             // Act
             _alarm.Check();
 
+            // Assert
+            Assert.IsFalse(_alarm.AlarmOn);
+        }
+
+        [Test]
+        [Category("SetAlarmOff")]
+        public void Check_PressureNormalAfterTooHigh_ShouldNotSetAlarm()
+        {
+            // Arrange
+            _sensor.PopNextPressurePsiValue().Returns(22);
+            _alarm.Check();
+            _sensor.PopNextPressurePsiValue().Returns(20);
+
+            // Act
+            _alarm.Check();
+
+            // Assert
+            Assert.IsFalse(_alarm.AlarmOn);
+        }
+
+        [Test]
+        [Category("SetAlarmOn")]
+        public void Check_PressureHighAfterNormal_ShouldSetAlarmOn()
+        {
+            // Arrange
+            _sensor.PopNextPressurePsiValue().Returns(20);
+            _alarm.Check();
+            _sensor.PopNextPressurePsiValue().Returns(22);
+
+            // Act
+            _alarm.Check();
+
+            // Assert
+            Assert.IsTrue(_alarm.AlarmOn);
+        }
+
+        [Test]
+        [Category("SetAlarmOff")]
+        public void AlarmOn_Initially_ShouldBeFalse()
+        {
             // Assert
             Assert.IsFalse(_alarm.AlarmOn);
         }
